eb0b7d81a2da519acf80a22b1a364ba08e07e059
Max <bilousmaks27@gmail.com>
2024-11-18 23:51:02 +0200

    New

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Імпортуй HomeComponent
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Головна сторінка
+];
